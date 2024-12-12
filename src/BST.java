750230a29ca8d91eb600a23ab82d8d04931b4dad
jaredsaal <jared.saal@menloschool.org>
2024-12-12 11:44:49 -0800

    Finished all functions (and comments)

--- src/BST.java
+++ src/BST.java
@@ -47,20 +47,24 @@ public class BST {
      * @return true if val is in the tree, false otherwise
      */
     public boolean search(int val) {
-        // TODO: Complete the search function
+        // Returns true if number is in tree and false if number isn't
         return searchHelper(root, val);
     }
 
     public boolean searchHelper(BSTNode root, int val) {
+        // If the end of the branch is reached, return false
         if (root == null) {
             return false;
         }
-        if (val == root.getVal()) {
+        // If the root is equal to the value, return true!
+        else if (val == root.getVal()) {
             return true;
         }
-        if (val < root.getVal()) {
+        // Case 1: Value is less than root - go to left child
+        else if (val < root.getVal()) {
             return searchHelper(root.getLeft(), val);
         }
+        // Case 2: Value is more than root - go to right child
         else {
             return searchHelper(root.getRight(), val);
         }
@@ -70,20 +74,23 @@ public class BST {
      * @return ArrayList of BSTNodes in inorder
      */
     public ArrayList<BSTNode> getInorder() {
-        // TODO: Complete inorder traversal
+        // Initialize array of nodes
         ArrayList<BSTNode> nodes = new ArrayList<BSTNode>();
-        inOrderHelper(nodes, root);
-        return nodes;
+        // Returns the final array in order
+        return inOrderHelper(nodes, root);
     }
 
     public ArrayList<BSTNode> inOrderHelper(ArrayList<BSTNode> list, BSTNode root) {
+        // If current root is null, return the list
         if (root == null) {
             return list;
         }
+        // If root has no children, add the root to list
         if (root.getLeft() == null && root.getRight() == null) {
             list.add(root);
             return list;
         }
+        // In order - left child, root, right child
         inOrderHelper(list, root.getLeft());
         list.add(root);
         inOrderHelper(list, root.getRight());
@@ -94,17 +101,19 @@ public class BST {
      * @return ArrayList of BSTNodes in preorder
      */
     public ArrayList<BSTNode> getPreorder() {
-        // TODO: Complete preorder traversal
+        // Initialize array of nodes
         ArrayList<BSTNode> nodes = new ArrayList<BSTNode>();
-        preOrderHelper(nodes, root);
-        return nodes;
+        // Returns the final array in preOrder
+        return preOrderHelper(nodes, root);
     }
 
     public ArrayList<BSTNode> preOrderHelper(ArrayList<BSTNode> list, BSTNode root) {
+        // If root has no childre, add root
         if (root.getLeft() == null && root.getRight() == null) {
             list.add(root);
             return list;
         }
+        // In preorder: root, left child, right child
         list.add(root);
         preOrderHelper(list, root.getLeft());
         preOrderHelper(list, root.getRight());
@@ -115,17 +124,19 @@ public class BST {
      * @return ArrayList of BSTNodes in postorder
      */
     public ArrayList<BSTNode> getPostorder() {
-        // TODO: Complete postorder traversal
+        // Initialize array of nodes
         ArrayList<BSTNode> nodes = new ArrayList<BSTNode>();
-        postOrderHelper(nodes, root);
-        return nodes;
+        // Returns arraylist in postOrder
+        return postOrderHelper(nodes, root);
     }
 
     public ArrayList<BSTNode> postOrderHelper(ArrayList<BSTNode> list, BSTNode root) {
+        // If root has no childre, add to list
         if (root.getLeft() == null && root.getRight() == null) {
             list.add(root);
             return list;
         }
+        // In postOrder - left, right, root
         postOrderHelper(list, root.getLeft());
         postOrderHelper(list, root.getRight());
         list.add(root);
@@ -139,10 +150,11 @@ public class BST {
      * @param val The value ot insert
      */
     public void insert(int val) {
-        // TODO: Complete insert
+        // if tree is empty, add node
         if (root == null) {
             root = new BSTNode(val);
         }
+        // Otherwise, traverse and insert
         else {
             rootHelper(root, val);
         }
@@ -150,17 +162,21 @@ public class BST {
 
     public void rootHelper(BSTNode root, int val) {
         if (val < root.getVal()) {
+            // If val is less than root and no left child, add node with value val
             if (root.getLeft() == null) {
                 root.setLeft(new BSTNode(val));
             }
+            // If val is less than root and has a child, go down and left
             else {
                 rootHelper(root.getLeft(), val);
             }
         }
         if (val > root.getVal()) {
+            // If val is greater than root and no right child, add node with value val
             if (root.getRight() == null) {
                 root.setRight(new BSTNode(val));
             }
+            // If val is greater than root and has a child, go down and right
             else {
                 rootHelper(root.getRight(), val);
             }
